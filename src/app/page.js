112d3809add66b5ca89370a2116b2fa0693982a2
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-17 17:00:04 +0600

    Status section desing started

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Home/Banner";
+import Status from "@/components/Home/Status";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Banner />
+        <Status />
       </main>
     </div>
   );
---
+++ src/components/Home/Status.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Status = () => {
+  return (
+    <div>
+      <p>status</p>
+    </div>
+  );
+};
+
+export default Status;
