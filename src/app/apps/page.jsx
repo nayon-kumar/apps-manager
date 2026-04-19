8a9787c4e6e7897a93dc4888f6600bca619f239c
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-19 18:20:13 +0600

    All apps page designed

--- src/app/apps/page.jsx
+++ src/app/apps/page.jsx
@@ -1,5 +1,6 @@
 import MyContainer from "@/components/Shared/MyContainer";
-import React from "react";
+import AppCard from "@/ui/AppCard";
+import React, { use } from "react";
 
 const appsPromise = fetch("http://localhost:3000/data.json").then((res) =>
   res.json(),
@@ -17,6 +18,16 @@ const AppsPage = () => {
           <p className="mt-4 text-[#627382]">
             Explore All Apps on the Market developed by us. We code for Millions
           </p>
+        </div>
+        <div className="pb-20">
+          <h3 className="font-semibold text-[#001931] mt-10  text-2xl">
+            ({allApps.length}) Apps Found
+          </h3>
+          <div className="grid mt-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {allApps.map((app) => (
+              <AppCard key={app.id} app={app} />
+            ))}
+          </div>
         </div>
       </MyContainer>
     </div>
